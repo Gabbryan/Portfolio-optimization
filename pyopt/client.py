236797a72f5c640f93f551a830a768d2b0a1e8d8
Gabriel Muratori <ryan.mrt01@gmail.com>
2023-12-16 12:57:07 +0100

    port opt

--- pyopt/client.py
+++ pyopt/client.py
@@ -1,145 +1,149 @@
 import requests
-import pandas as pd
-
-from typing import List
-from typing import Dict
-from typing import Union
-from datetime import date
 from datetime import datetime
-from datetime import timedelta
-from dateutil.relativedelta import *
-
-from fake_useragent import UserAgent
+import pandas as pd
+from typing import List, Dict
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import os
+import json
+from sklearn.cluster import KMeans
+from scipy import optimize 
 
 class PriceHistory():
 
-    """This is a simple Class object for scraping 
-    price data from the NASDAQ website."""
-
-    def __init__(self, symbols: List[str], user_agent: UserAgent) -> None:
-        """Initalizes the PriceHistory client.
-
-        ### Arguments:
-        ----
-        symbols (List[str]): A list of ticker symbols to pull 
-            quotes for.
-        """              
-
-        self._api_url = 'https://api.nasdaq.com/api/quote'
-        self._api_service = 'historical'
+    def __init__(self, symbols: List[str], api_key: str) -> None:
+        self._api_url = 'https://www.alphavantage.co/query'
         self._symbols = symbols
-        self.user_agent = user_agent
+        self._api_key = api_key
         self.price_data_frame = self._build_data_frames()
 
-    def _build_url(self, symbol: str) -> str:
-        """Builds a Full URL.
-
-        ### Arguments:
-        ----
-        symbol (str): The symbol you want to build a URL for.
-
-        ### Returns:
-        ----
-        str: A URL to the Ticker symbol provided.
-        """
-
-        parts = [self._api_url, symbol, self._api_service]
-        return '/'.join(parts)
-
-    @property
-    def symbols(self) -> List[str]:
-        """Returns all the symbols currently being pulled.
-
-        ### Returns:
-        ----
-        List[str]: A list of ticker symbols.
-        """        
-        return self._symbol
-    
     def _build_data_frames(self) -> pd.DataFrame:
-        """Builds a data frame with all the price data.
-
-        ### Returns:
-        ----
-        pd.DataFrame: A Pandas DataFrame with the data cleaned
-            and sorted.
-        """        
-
         all_data = []
-        to_date = datetime.today().date()
-
-        # Calculate the Start and End Point.
-        from_date = to_date - relativedelta(months=6)
 
         for symbol in self._symbols:
-
-            all_data = self._grab_prices(
-                symbol=symbol,
-                from_date=from_date,
-                to_date=to_date
-            ) + all_data
-        
+            all_data += self._grab_prices(symbol)
         price_data_frame = pd.DataFrame(data=all_data)
         price_data_frame['date'] = pd.to_datetime(price_data_frame['date'])
-    
         return price_data_frame
 
-    def _grab_prices(self, symbol: str, from_date: date, to_date: date) -> List[Dict]:
-        """Grabs the prices.
-
-        ### Arguments:
-        ----
-        symbol (str): The symbol to pull prices for.
-        
-        from_date (date): The starting date to pull prices.
-        
-        to_date (date): The ending data to pull prices for.
-
-        ### Returns:
-        ----
-        List[Dict]: A list of candle dictionaries.
-        """        
-        
-        # Build the URL.
-        price_url = self._build_url(symbol=symbol)
-
-        # Calculate the limit.
-        limit: timedelta = (to_date - from_date)
-
-        # Define the parameters.
+    def _grab_prices(self, symbol: str) -> List[Dict]:
         params = {
-            'fromdate': from_date.isoformat(),
-            'todate': to_date.isoformat(),
-            'assetclass': 'stocks',
-            'limit': limit.days
+            'function': 'TIME_SERIES_DAILY',
+            'symbol': symbol,
+            'apikey': self._api_key,
         }
 
-        # Fake the headers.
-        headers = {
-            'user-agent': self.user_agent
-        }
+        response = requests.get(self._api_url, params=params)
 
-        # Grab the historical data.
-        historical_data = requests.get(
-            url=price_url,
-            params=params,
-            headers=headers,
-            verify=True
-        )
+        if response.status_code == 200:
+            data = response.json()
+            if 'Time Series (Daily)' in data:
+                time_series = data['Time Series (Daily)']
+                historical_data = []
+                for date_str, values in time_series.items():
+                    date = datetime.strptime(date_str, '%Y-%m-%d')
+                    row = {
+                        'date': date,
+                        'symbol': symbol,
+                        'open': float(values['1. open']),
+                        'high': float(values['2. high']),
+                        'low': float(values['3. low']),
+                        'close': float(values['4. close']),
+                        'volume': int(values['5. volume']),
+                    }
+                    historical_data.append(row)
+                return historical_data
 
-        # If it's okay parse it.
-        if historical_data.ok:
-            historical_data = historical_data.json()
-            historical_data = historical_data['data']['tradesTable']['rows']
+        return []
 
-            # Clean the data.
-            for table_row in historical_data:
-                table_row['symbol'] = symbol
-                table_row['close'] = float(table_row['close'].replace('$',''))
-                table_row['volume'] = int(table_row['volume'].replace(',',''))
-                table_row['open'] = float(table_row['open'].replace('$',''))
-                table_row['high'] = float(table_row['high'].replace('$',''))
-                table_row['low'] = float(table_row['low'].replace('$',''))
+def StockReturnsComputing(StockPrice):
+    import numpy as np
 
-            return historical_data
+    # Convertir le DataFrame Pandas en un array NumPy
+    StockPriceArray = StockPrice.values
 
+    # Récupérer le nombre de lignes et de colonnes
+    Rows, Columns = StockPriceArray.shape
+
+    # Initialiser le tableau de rendements
+    StockReturn = np.zeros([Rows-1, Columns])
+
+    # Calculer les rendements
+    for j in range(Columns):        # j: Assets
+        for i in range(Rows-1):     # i: Daily Prices
+            StockReturn[i, j] = ((StockPriceArray[i+1, j] - StockPriceArray[i, j]) / StockPriceArray[i, j]) * 100
+
+    return StockReturn
+
+#function obtains maximal return portfolio using linear programming
+
+def MaximizeReturns(MeanReturns, PortfolioSize):
+    
+    #dependencies
+    from scipy.optimize import linprog
+    
+    c = (np.multiply(-1, MeanReturns))
+    A = np.ones([PortfolioSize,1]).T
+    b=[1]
+    res = linprog(c, A_ub = A, b_ub = b, bounds = (0,1), method = 'simplex') 
+    
+    return res
+#function obtains minimal risk portfolio 
+
+def MinimizeRisk(CovarReturns, PortfolioSize):
+    
+    def  f(x, CovarReturns):
+        func = np.matmul(np.matmul(x, CovarReturns), x.T) 
+        return func
+
+    def constraintEq(x):
+        A=np.ones(x.shape)
+        b=1
+        constraintVal = np.matmul(A,x.T)-b 
+        return constraintVal
+    
+    xinit=np.repeat(0.1, PortfolioSize)
+    cons = ({'type': 'eq', 'fun':constraintEq})
+    lb = 0
+    ub = 1
+    bnds = tuple([(lb,ub) for x in xinit])
+    
+    opt = optimize.minimize (f, x0 = xinit, args = (CovarReturns),  bounds = bnds, \
+                             constraints = cons, tol = 10**-3)
+    
+    return opt
+
+#function obtains Minimal risk and Maximum return portfolios
+
+def MinimizeRiskConstr(MeanReturns, CovarReturns, PortfolioSize, R):
+    
+    def  f(x,CovarReturns):
+         
+        func = np.matmul(np.matmul(x,CovarReturns ), x.T)
+        return func
+
+    def constraintEq(x):
+        AEq=np.ones(x.shape)
+        bEq=1
+        EqconstraintVal = np.matmul(AEq,x.T)-bEq 
+        return EqconstraintVal
+    
+    def constraintIneq(x, MeanReturns, R):
+        AIneq = np.array(MeanReturns)
+        bIneq = R
+        IneqconstraintVal = np.matmul(AIneq,x.T) - bIneq
+        return IneqconstraintVal
+    
+
+    xinit=np.repeat(0.1, PortfolioSize)
+    cons = ({'type': 'eq', 'fun':constraintEq},
+            {'type':'ineq', 'fun':constraintIneq, 'args':(MeanReturns,R) })
+    lb = 0
+    ub = 1
+    bnds = tuple([(lb,ub) for x in xinit])
+
+    opt = optimize.minimize (f, args = (CovarReturns), method ='trust-constr',  \
+                        x0 = xinit,   bounds = bnds, constraints = cons, tol = 10**-3)
+    
+    return  opt
